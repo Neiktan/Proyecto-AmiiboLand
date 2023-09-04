4b6168cf88c8b98c0dc906acf38b822b53ad81d8
Neiktan <wichoalguiar@gmail.com>
2023-09-04 01:02:57 -0500

    agregado de carousel y modo

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-//agregar el boton para cambio de fondo
-
-
 const cardsAmiibo = data => {
     const cards = data.reduce((acc, element) => {
         return acc + `
@@ -13,7 +9,7 @@ const cardsAmiibo = data => {
                 </figure>
             ${element.name}
                 <div>
-                    <button>Agregar al carrito</button>
+                    <button class="btn-box">Agregar al carrito</button>
                 </div>
             </div>
         `
@@ -21,21 +17,45 @@ const cardsAmiibo = data => {
     document.querySelector(".cartas").innerHTML = cards
 }
 
+const agregarAlCarrito = (nodos, array) => {
+    for (let i = 0; i < nodos.length; i++){
+        nodos[i].onclick = (e) => {
+            const name = e.currentTarget.name.slice(7)
+            const buscarProducto = array.find(element => element.name === name)
+            carrito.push(buscarProducto)
+            localStorage.setItem("productos", JSON.stringify(carrito))
+        }
+    }
+}
+
+eventoCartas(todasCartas, productos)
+
 fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
     .then(res => res.json())
     .then(data => {
-        cardsAmiibo(data.amiibo)
+        cardsAmiibo(data.amiibo) //DOM dentro del then
     })
 
 //fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
 //.then( res => res.json())
 //.then( data => console.log(data))
 
+//Modo Oscuro
+let toggle = document.getElementById('container');
+let body = document.querySelector('body')
+
+toggle.onclick = function(){
+toggle.classList.toggle('active');
+body.classList.toggle('active');
+}
+
+
+//Carousel
 let swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
+        delay: 5000,
         disableOnInteraction: false,
     },
     pagination: {
