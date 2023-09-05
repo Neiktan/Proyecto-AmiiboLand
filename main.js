a4a22af8f8a5648a397a893041c517273436b17d
Neiktan <wichoalguiar@gmail.com>
2023-09-04 21:02:32 -0500

    act 9/4

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const cardsAmiibo = data => {
     const cards = data.reduce((acc, element) => {
         return acc + `
             <div class="cartas-amiibo">
-            <h2>${element.amiiboSeries}</h2>
-            <h3>${element.character}</h3>
+            <h2 class="">${element.amiiboSeries}</h2>
                 <figure>
                     <img src="${element.image}" alt="${element.name}" class="cartas-productos">
                 </figure>
@@ -17,18 +16,23 @@ const cardsAmiibo = data => {
     document.querySelector(".cartas").innerHTML = cards
 }
 
-const agregarAlCarrito = (nodos, array) => {
+const agregarAlCarrito = (nodos, amiibo) => {
+
+    const agregar = document.querySelectorAll(".btn-box")
+
     for (let i = 0; i < nodos.length; i++){
         nodos[i].onclick = (e) => {
-            const name = e.currentTarget.name.slice(7)
-            const buscarProducto = array.find(element => element.name === name)
-            carrito.push(buscarProducto)
-            localStorage.setItem("productos", JSON.stringify(carrito))
+            console.log("me hacen click")
+            console.log(e.target.name)
+            //const name = e.currentTarget.name.slice(7)
+            //const buscarProducto = array.find(element => element.name === name)
+            //carrito.push(buscarProducto)
+            //localStorage.setItem("productos", JSON.stringify(carrito))
         }
     }
 }
 
-eventoCartas(todasCartas, productos)
+//eventoCartas(todasCartas,  character)
 
 fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
     .then(res => res.json())
@@ -36,35 +40,40 @@ fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
         cardsAmiibo(data.amiibo) //DOM dentro del then
     })
 
-//fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
-//.then( res => res.json())
-//.then( data => console.log(data))
+fetch(`https://www.amiiboapi.com/api/amiibo/?pageSize=25&page=1`)
+.then( res => res.json())
+.then( data => console.log(data))
+
+
+
 
 //Modo Oscuro
 let toggle = document.getElementById('container');
 let body = document.querySelector('body')
 
-toggle.onclick = function(){
-toggle.classList.toggle('active');
-body.classList.toggle('active');
-}
+//toggle.onclick = function(){
+//toggle.classList.toggle('active');
+//body.classList.toggle('active');
+//}
+
+
 
 
 //Carousel
-let swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+//let swiper = new Swiper(".mySwiper", {
+    //spaceBetween: 30,
+    //centeredSlides: true,
+    //autoplay: {
+        //delay: 5000,
+        //disableOnInteraction: false,
+    //},
+    //pagination: {
+        //el: ".swiper-pagination",
+        //clickable: true,
+    //},
+    //navigation: {
+        //nextEl: ".swiper-button-next",
+        //prevEl: ".swiper-button-prev",
+    //},
+//});
 
