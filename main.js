32d06fddad1751314fb9e0947b24274c6e8aae54
Neiktan <wichoalguiar@gmail.com>
2023-09-05 20:30:47 -0500

    falta de JSON

--- main.js
+++ main.js
@@ -60,20 +60,20 @@ let body = document.querySelector('body')
 
 
 //Carousel
-//let swiper = new Swiper(".mySwiper", {
-    //spaceBetween: 30,
-    //centeredSlides: true,
-    //autoplay: {
-        //delay: 5000,
-        //disableOnInteraction: false,
-    //},
-    //pagination: {
-        //el: ".swiper-pagination",
-        //clickable: true,
-    //},
-    //navigation: {
-        //nextEl: ".swiper-button-next",
-        //prevEl: ".swiper-button-prev",
-    //},
-//});
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
